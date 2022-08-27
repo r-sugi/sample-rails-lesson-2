75f12084fbf7eb968210925d9348e6debab98edd
r-sugi <r.sugimoto0615@gmail.com>
2022-08-28 04:00:52 +0900

    add: user delete

--- be/db/seeds.rb
+++ be/db/seeds.rb
@@ -8,13 +8,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name:  "Example User",
-  email: "example@railstutorial.org",
+  email: "example@example.com",
   password:              "foobar",
-  password_confirmation: "foobar")
+  password_confirmation: "foobar",
+  admin: true)
 
 99.times do |n|
 name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
+email = "example-#{n+1}@example.com"
 password = "password"
 User.create!(name:  name,
     email: email,
