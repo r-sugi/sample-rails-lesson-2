d236135773473b3e88a7c1d25641d899a82bef81
r-sugi <r.sugimoto0615@gmail.com>
2022-08-28 05:27:08 +0900

    add: selling_imageの削除

--- be/app/controllers/selling_images_controller.rb
+++ be/app/controllers/selling_images_controller.rb
@@ -1,5 +1,6 @@
 class SellingImagesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @selling_image = current_user.selling_images.build(selling_image_params)
@@ -12,11 +13,20 @@ class SellingImagesController < ApplicationController
   end
 
   def destroy
+    @selling_image.destroy!
+    flash[:success] = "SellingImage deleted"
+    redirect_to request.referrer
   end
+
 
   private
 
     def selling_image_params
       params.require(:selling_image).permit(:content)
+    end
+
+    def correct_user
+      @selling_image = current_user.selling_images.find_by(id: params[:id])
+      redirect_to root_url if @selling_image.nil?
     end
 end
