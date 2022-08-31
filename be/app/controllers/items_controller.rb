57adbebaf34c00fed839aeedf05583dd4826ec28
r-sugi <r.sugimoto0615@gmail.com>
2022-08-31 17:16:04 +0900

    fix: 非会員の場合は保存しない

--- be/app/controllers/items_controller.rb
+++ be/app/controllers/items_controller.rb
@@ -4,12 +4,15 @@ class ItemsController < ApplicationController
     @selling_image = SellingImage.find(params[:id])
 
     # 閲覧回数の保存
-    latestViews = UserItemView.where(user_id: current_user.id, selling_image_id: @selling_image.id).order(created_at: :desc).limit(1)
 
-    if latestViews.exists?
-      UserItemView.create(user_id: current_user.id, selling_image_id: @selling_image.id) unless latestViews[0].created_at.day == Time.zone.now.day
-    else
-      UserItemView.create(user_id: current_user.id, selling_image_id: @selling_image.id)
+    if current_user.present?
+      latestViews = UserItemView.where(user_id: current_user.id, selling_image_id: @selling_image.id).order(created_at: :desc).limit(1)
+
+      if latestViews.exists?
+        UserItemView.create(user_id: current_user.id, selling_image_id: @selling_image.id) unless latestViews[0].created_at.day == Time.zone.now.day
+      else
+        UserItemView.create(user_id: current_user.id, selling_image_id: @selling_image.id)
+      end
     end
   end
 
